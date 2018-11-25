fa51b792d265315e88ac47f7ac30d1163a9dab7e
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-11-25 23:03:28 +0100

    Insert alert dialog to alerting the provider before removing the product

--- front-end/productslist/productslist.js
+++ front-end/productslist/productslist.js
@@ -18,15 +18,7 @@ $(document).ready(function(){
         //TODO: validate form and send to the web server.
     }
 
-    function saveModify(){
-        $(this).siblings().prop("disabled", false);
-        $(this).siblings().children().prop("disabled", false);
-        $(this).prevAll().show();
-        $(this).hide();
-
-        //TODO: validate and send to web server
-    }
-
+    /* Modify product */
     function modifyProduct(){
         var siblings = $(this).siblings()
         siblings.prop("disabled", false);
@@ -36,10 +28,32 @@ $(document).ready(function(){
         $(this).next().next().show();
     }
     
+    /* Save changes written with modify */
+    function saveModify(){
+        $(this).siblings().prop("disabled", false);
+        $(this).siblings().children().prop("disabled", false);
+        $(this).prevAll().show();
+        $(this).hide();
+
+        //TODO: validate and send to web server
+    }
+
+    /* Remove product */
+    function removeProduct(){
+        //show a dialog to alert provider.
+        if (confirm("Are you sure to delete the product?")) {
+            txt = "You pressed OK!";
+            $(this).parent().hide();
+            //TODO: delete the product from db
+        }
+        
+    }
+
     /* Activate listener from begin*/
     $("#addProduct").click(addProduct);
     $(".cancel").click(cancel);
     $(".saveProducts").click(saveProduct);
     $(".modify").click(modifyProduct);
     $(".saveModify").click(saveModify);
+    $(".remove").click(removeProduct)
 });
