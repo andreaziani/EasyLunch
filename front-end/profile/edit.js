67dd9b59aff1da1b37f0d04181b2dc8c87cf20b3
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-12-10 23:45:49 +0100

    Validation of client profile form

--- front-end/profile/edit.js
+++ front-end/profile/edit.js
@@ -1,27 +1,70 @@
 /* @flow */
-$(document).ready(function () {
-    var hiddenButton = $(".hiddenButton");
-
-    /* Enable information editing */
-    function editInformation(){
-        var input = $(this).prev().children();
-        if(input.is(":disabled")){
-            input.prop("disabled", false);
-            input.val(" ");
-            hiddenButton.fadeIn();
+$(document).ready(function() {
+  var hiddenButton = $(".hiddenButton");
+  
+  // check if value is not equal to params passed
+  function check(value, element, param) {
+    var notEqual = true;
+    for (i = 0; i < param.length; i++) {
+        if (value.toLowerCase().includes(param[i])) { notEqual = false; }
+    }
+    return this.optional(element) || notEqual;
+  }
+  /* Enable information editing */
+  function editInformation() {
+      var input = $(this)
+      .prev()
+      .children();
+      if (input.is(":disabled")) {
+          input.prop("disabled", false);
+          input.val(" ");
+          hiddenButton.fadeIn();
         }
     }
-
+    
     /* Save the modified information */
-    function saveChanges(){
-        // TODO: validate the modified info.
+    function saveChanges() {
         // TODO: send the modified info to webserver.
-        var modifiedInfo = $("input:not(:disabled):not(#saveChanges)");
-        for(var i = 0; i < modifiedInfo.length; i++){
-            modifiedInfo.eq(i).val();
-        }
+        $("form").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3,
+                    notEqualTo: ['select', 'alter', 'update', 'delete']
+                },
+                surname: {
+                    required: true,
+                    minlength: 3,
+                    notEqualTo: ["select", "alter", "update", "delete"]
+                },
+                password: {
+                    required: true,
+                    minlength: 3,
+                    notEqualTo: ["select", "alter", "update", "delete"]
+                },
+                birthdate: {
+                    required: true,
+                    date: true
+                },
+                telephone: {
+                    required: true,
+                    number: true,
+                    minlength: 5,
+                    maxlength: 10
+                },
+                email: {
+                    required: true,
+                    email: true
+                }
+            }
+        });
     }
+    //add jQuery validator rule 
+    jQuery.validator.addMethod("notEqualTo", check, "Please enter a diferent value.");
 
-    $("a.edit").click(editInformation); /*On click of edit icon, enable edit information */
-    $("#saveChanges").click(saveChanges);
-});
+    $("a.edit").click(
+        editInformation
+        ); /*On click of edit icon, enable edit information */
+        $("#saveChanges").click(saveChanges);
+    });
+    
