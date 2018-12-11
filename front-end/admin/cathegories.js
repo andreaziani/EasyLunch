5bfbe7d675907a20724227c5ce8f44269e8f8c6d
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-12-11 22:01:16 +0100

    Create an admin's page to add and view cathegories

---
+++ front-end/admin/cathegories.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    var addCathegoryButton = $("#addCathegory");
+    var cancelButton = $("#cancel");
+    var saveButton = $("#save");
+    var invalidStrings = ['select', 'alter', 'update', 'delete'];
+   
+    /* Validation starts here */
+    function check(value, element, param) {
+        var notEqual = true;
+        for (var i = 0; i < param.length; i++) {
+            if (value.toLowerCase().includes(param[i])) { notEqual = false; } // check if value is not equal to params passed
+        }
+        return this.optional(element) || notEqual;
+    }
+    /* Validation function */
+    function validation(){
+        $("form").validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3,
+                    notEqualTo: invalidStrings
+                },
+                id: {
+                    required: true,
+                    minlength: 1,
+                    notEqualTo: invalidStrings
+                }
+            }
+        });
+    }
+    /* Hide the form for adding new product */
+    function cancel(e){
+        e.preventDefault();
+        $(".hidden").hide();
+        $(".error:not(input, textarea)").hide();
+        addCathegoryButton.show();
+    }
+
+    function add(e) { 
+        e.preventDefault();
+        $(".hidden").fadeIn();
+        addCathegoryButton.hide();
+    };    
+    /* Save the cathegory */
+    function saveCathegory(){
+        validation();
+    }
+
+    //add jQuery validator rule 
+    jQuery.validator.addMethod("notEqualTo", check, "Please enter a diferent value.");
+    addCathegoryButton.click(add);
+    cancelButton.click(cancel);
+    saveButton.click(saveCathegory);
+});
