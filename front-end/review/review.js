d96efb7cbf30fd37c330e6008e04c7999c699d0d
Simone <simone.barbaro2@studio.unibo.it>
2018-12-14 12:16:02 +0100

    added review stars

---
+++ front-end/review/review.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $("form").validate({
+        rules: {
+            rank: {
+                required: true
+            }
+        }
+    });
+
+    $.fn.customStarRatings = function(obj) {
+        var wrapper = this;
+        var stars = obj.childClass;
+        var input = obj.inputClass;
+        $(wrapper).hover(function() {
+            activeClassCount = $(this).find('.rating').index() + 1;
+        }, function() {
+            var $this = $(this);
+            $this.find(stars).slice(1, activeClassCount).addClass('is-active');
+            $this.find(stars).slice(activeClassCount, 10).not('.rating').removeClass('is-active');
+        });
+        $(stars).hover(function() {
+            $(this).prevAll(stars).add($(this)).addClass('is-active');
+            $(this).nextAll(stars).removeClass('is-active');
+        });
+        $(stars).click(function(event) {
+            $(".rating").removeClass("rating");
+            $(this).addClass('rating');
+            activeClassCount = $(this).index() + 1;
+            $(this).prevAll(stars).addClass('is-active');
+            if (input) {
+                $(input).val(($(this).index() + 1)/2);
+                console.log($(input).val(), activeClassCount);
+            }
+        });
+    }
+    
+    $(".c-rating").customStarRatings({
+        childClass: ".c-rating__item",
+        inputClass: "#rank"
+    });
+});
