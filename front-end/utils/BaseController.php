64315ab0d45481fb71aca8b8f6f1d4d1a91dafaa
Andrea Ziani <andrea.ziani@studio.unibo.it>
2018-12-15 14:10:07 +0100

    Creation of DBManager

--- front-end/utils/BaseController.php
+++ front-end/utils/BaseController.php
@@ -1,4 +1,8 @@
 <?php 
+    /**
+     * Class that manage the path for php files. 
+     * For a correct usage, create in hodocs the folder "progetto" and put "fron-end" inside of it 
+     */
     class BaseController {
         private $webSitePath;
 
@@ -6,8 +10,8 @@
             $this->webSitePath = $_SERVER['DOCUMENT_ROOT'] . '/progetto/front-end/';
         }
         /**
-         * Require one the file written in filePath
-         * @param filepath the local path of the file to require
+         * Require in the page the file written in filePath
+         * @param filepath the local path (after "front-end") of the required file
          */
         public function requireFromWebSitePath($filePath){
             require_once $this->webSitePath . $filePath;
---
+++ front-end/utils/DBManager.php
@@ -0,0 +1,36 @@
+<?php 
+    /**
+     * Class to manage the db connection and queries.
+     */
+    class DBManager {
+        private $con;
+        private $server = "localhost";
+        private $user = "root";
+        private $pass = "";
+        private $dbname = "progetto";
+
+        
+        public function _construct(){
+            $this->con = mysqli($server, $user, $pass, $dbname);
+            //check connection 
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            } 
+        }
+
+        /**
+         * Get the db connection.
+         */
+        public function getConnection(){
+            return $this->con;
+        }
+
+        /**
+         * Close the connection with db.
+         */
+        public function closeConnection(){
+            $this->conn->close();
+        }
+    }
+
+?>
