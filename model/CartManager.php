5a2873d08c22ca683807953d4bb41be84f15afea
Simone <simone.barbaro2@studio.unibo.it>
2018-12-26 18:22:35 +0100

    corrected cart loading

--- model/CartManager.php
+++ model/CartManager.php
@@ -12,7 +12,7 @@ class CartManager
     }
 
     public function getCart($id) {
-        return new Cart($this->queryManager->searchByKey("Carts", "Id", $id)); //TODO cartEntries
+        return new Cart($this->queryManager->searchByKey("Carts", "Id", $id), $this->queryManager->searchByAttribute("CartData", "CartId", $id));
     }
 
     public function findOrCreateOrder($cart, $providerId) {
