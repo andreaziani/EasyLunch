f1adc64fea9420431ef8e44c35486fc4a9a01e5d
Simone <simone.barbaro2@studio.unibo.it>
2019-01-18 09:20:24 +0100

    typing error

--- view/template/header/_providerheader.php
+++ view/template/header/_providerheader.php
@@ -1,4 +1,4 @@
-<a href="/ProgettoTecWeb/view/template/notification/notificationPage.php""><img src="/ProgettoTecWeb/images/icons/bell.png" alt="notifications" width="70" id="bell"/></a>
+<a href="/ProgettoTecWeb/view/template/notification/notificationPage.php"><img src="/ProgettoTecWeb/images/icons/bell.png" alt="notifications" width="70" id="bell"/></a>
 <!-- from here -->
 <a href="/ProgettoTecWeb/view/template/profile/providerProfile.php">Profile</a>
 <a href="/ProgettoTecWeb/view/template/providerproductslist/providerProductsList.php">Personal Shop</a>
