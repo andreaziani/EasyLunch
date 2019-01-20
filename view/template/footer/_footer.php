81f55448248c5cc9a4761ea3fec504a4f55f290a
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-01-20 20:31:54 +0100

    Mail if support required

--- view/template/footer/_footer.php
+++ view/template/footer/_footer.php
@@ -1,10 +1,10 @@
-<?php ?>
+<script src="/ProgettoTecWeb/view/template/footer/footer.js"></script>
 <footer class="footer container-fluid text-center">
             <p class="mb-1">© 2018-2019 Easy Lunch</p>
             <ul class="list-inline">
                 <li class="list-inline-item"><a href="#">Privacy</a></li>
                 <li class="list-inline-item"><a href="#">Terms</a></li>
-                <li class="list-inline-item"><a href="#">Support</a></li>
+                <li class="list-inline-item"><a href="#" class='support'>Support</a></li>
             </ul>
 </footer>
 </body>
