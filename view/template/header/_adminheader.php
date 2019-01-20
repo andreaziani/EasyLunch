719fd561b977194a4d0ab1502b45db0e0172f5f5
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-01-20 15:43:51 +0100

    Included bootstrap in admin header

--- view/template/header/_adminheader.php
+++ view/template/header/_adminheader.php
@@ -1,4 +1,16 @@
 <script src="/ProgettoTecWeb/view/template/header/header.js"></script>
-<a href="/ProgettoTecWeb/view/template/clientproviderslist/clientproviderslist.php">Shop</a>
-<a href="/ProgettoTecWeb/view/template/admin/categories.php">Categories</a>
-<a href="/ProgettoTecWeb/controller/action/logout.php">Logout</a>
+<div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+    </button>
+</div>
+<script src="/ProgettoTecWeb/view/template/header/header.js"></script>
+<div class="collapse navbar-collapse" id="myNavbar">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="/ProgettoTecWeb/view/template/clientproviderslist/clientproviderslist.php">Shop</a></li>
+            <li><a href="/ProgettoTecWeb/view/template/admin/categories.php">Categories</a></li>
+            <li><a href="/ProgettoTecWeb/controller/action/logout.php">Logout</a></li>
+        </ul>
+</div>
