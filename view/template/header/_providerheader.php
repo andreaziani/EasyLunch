4aed6cb5e4559f289e3492d9436a816a840f4428
Andrea Ziani <andrea.ziani@studio.unibo.it>
2019-01-21 12:46:27 +0100

    Provider profile

--- view/template/header/_providerheader.php
+++ view/template/header/_providerheader.php
@@ -8,9 +8,9 @@
     </div>
 <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav navbar-nav navbar-right">
-        <li class="nav-list-item"><a href="/ProgettoTecWeb/view/template/providerproductslist/providerProductsList.php"><span class="glyphicon glyphicon-list-alt"></span> Your products</a></li>
+        <li class="nav-list-item"><a href="/ProgettoTecWeb/view/template/providerproductslist/providerProductsList.php"><span class="glyphicon glyphicon-list-alt"></span> Personal menu</a></li>
         <li class="nav-list-item"><a href="/ProgettoTecWeb/view/template/orders/orderPage.php"><span class="glyphicon glyphicon-pushpin"></span> Orders</a></li>
-        <li class="nav-list-item"><a href="/ProgettoTecWeb/view/template/profile/clientProfile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
+        <li class="nav-list-item"><a href="/ProgettoTecWeb/view/template/profile/providerProfile.php"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
         <li class="nav-list-item"><a href="/ProgettoTecWeb/controller/action/logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
     </ul>
 </div>
--- view/template/profile/providerProfile.php
+++ view/template/profile/providerProfile.php
@@ -9,56 +9,57 @@ use Utils\PathManager;
     $base->requireFromWebSitePath('header/_header.php');
 ?>
 <link rel="stylesheet" href="style.css">
-<div>
-    <img src="/ProgettoTecWeb/images/icons/user.png" alt="Personal profile" width="70">
-    <fieldset id="personalinfo">
-        <legend>Personal informations</legend>
-        <!--Span will be filled with text taken from db-->
-        <form id="profileInformations" action="/ProgettoTecWeb/controller/action/modifyProfile.php" method="POST">
+<div class='container'>
+    <div class='col-12 form'>
+            <h1 class='display-4'>Personal informations</h1>
+            
+            <div class="form-group">
+            <form id="profileInformations" action="/ProgettoTecWeb/controller/action/modifyProfile.php" method="POST">
             <?php 
-                echo "<label>Name: <input type='text' name='Name' id='name' value='" . $_SESSION["user"]->name .
+                echo "<label>Name: <input class='form-control input-sm' type='text' name='Name' id='name' value='" . $_SESSION["user"]->name .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Surname: <input type='text' name='Surname' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Surname: <input class='form-control input-sm' type='text' name='Surname' id='surname' value='" .
                 $_SESSION["user"]->surname .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Birthdate: <input type='text' name='Birthdate' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Birthdate: <input class='form-control input-sm' type='text' name='Birthdate' id='surname' value='" .
                 $_SESSION["user"]->birthdate .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Telephone: <input type='text' name='PhoneNumber' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Telephone: <input class='form-control input-sm' type='text' name='PhoneNumber' id='surname' value='" .
                 $_SESSION["user"]->phoneNumber .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Email: <input type='text' name='Email' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Email: <input class='form-control input-sm' type='text' name='Email' id='surname' value='" .
                 $_SESSION["user"]->email .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>PIVA: <input type='text' name='IVA' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>PIVA: <input class='form-control input-sm' type='text' name='IVA' id='surname' value='" .
                 $_SESSION["user"]->iva . 
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>City: <input type='text' name='CityAddress' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>City: <input class='form-control input-sm' type='text' name='CityAddress' id='surname' value='" .
                 $_SESSION["user"]->city . 
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Street: <input type='text' name='AddressStreet' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Street: <input class='form-control input-sm' type='text' name='AddressStreet' id='surname' value='" .
                 $_SESSION["user"]->street .
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" .
-                " <label>Number: <input type='text' name='AddressNumber' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" .
+                " <label>Number: <input class='form-control input-sm' type='text' name='AddressNumber' id='surname' value='" .
                 $_SESSION["user"]->number . 
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" . 
-                " <label>Company: <input type='text' name='CompanyName' id='surname' value='" .
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" . 
+                " <label>Company: <input class='form-control input-sm' type='text' name='CompanyName' id='surname' value='" .
                 $_SESSION["user"]->companyName . 
                 "' disabled /></label>" .
-                " <a class='edit'><img src='/ProgettoTecWeb/images/icons/edit.png' alt='modify' width='15'></a><br />" . 
-                " <button type='submit' id='saveChanges' class='hiddenButton'>Save changes</button>";
+                " <a class='edit'> <span class='glyphicon glyphicon-pencil'></span> </a><br />" . 
+                " <button type='submit' id='saveChanges' class='btn btn-primary input-sm hiddenButton'>Save changes</button>";
             ?>
         </form>
-    </fieldset>
+        </div>
+    </div>
 </div>
 <script src="editProfile.js"></script>
 <?php 
